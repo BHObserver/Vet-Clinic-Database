d79ff87a5d7021b78b238dc9d8f555a48e345ece
BHObserver <BHObserver0@gmail.com>
2023-11-10 01:53:18 +0600

    update files

--- data.sql
+++ data.sql
@@ -6,3 +6,32 @@ INSERT INTO ANIMALS
      (2, 'Gabumon', '2018-11-15', 2, TRUE, 8),
      (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04),
      (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+
+
+-- Animal 1
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Charmander', '2020-02-08', 11, FALSE, 0, 'Unknown');
+
+-- Animal 2
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Plantmon', '2021-11-15', 5.7, TRUE, 2, 'Unknown');
+
+-- Animal 3
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Squirtle', '1993-04-02', 12.13, FALSE, 3, 'Unknown');
+
+-- Animal 4
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Angemon', '2005-06-12', 45, TRUE, 1, 'Unknown');
+
+-- Animal 5
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Boarmon', '2005-06-07', 20.4, TRUE, 7, 'Unknown');
+
+-- Animal 6
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Blossom', '1998-10-13', 17, TRUE, 3, 'Unknown');
+
+-- Animal 7
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
+VALUES ('Ditto', '2022-05-14', 22, TRUE, 4, 'Unknown');
--- queries.sql
+++ queries.sql
@@ -8,35 +8,6 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-
--- Animal 1
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Charmander', '2020-02-08', 11, FALSE, 0, 'Unknown');
-
--- Animal 2
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Plantmon', '2021-11-15', 5.7, TRUE, 2, 'Unknown');
-
--- Animal 3
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Squirtle', '1993-04-02', 12.13, FALSE, 3, 'Unknown');
-
--- Animal 4
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Angemon', '2005-06-12', 45, TRUE, 1, 'Unknown');
-
--- Animal 5
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Boarmon', '2005-06-07', 20.4, TRUE, 7, 'Unknown');
-
--- Animal 6
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Blossom', '1998-10-13', 17, TRUE, 3, 'Unknown');
-
--- Animal 7
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) 
-VALUES ('Ditto', '2022-05-14', 22, TRUE, 4, 'Unknown');
-
 START TRANSACTION;
 UPDATE animals
     SET species = 'unspecified';
