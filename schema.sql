16fc7edc4b4bc62380a4b549aff8c4ef2880c403
BHObserver <BHObserver0@gmail.com>
2023-11-09 14:26:12 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE vet_clinic.animals (
-    id INT,
-    name VARCHAR(100),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg FLOAT
+    weight_kg DECIMAL(8, 2)
 );
