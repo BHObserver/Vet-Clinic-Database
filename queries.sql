8f67a1bc41d8f31ea6c194c5ab6d4186ea72f970
BHObserver <BHObserver0@gmail.com>
2023-11-11 03:15:15 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -89,3 +89,7 @@ SELECT a.name
 FROM animals a
 JOIN species s ON a.species_id = s.id
 WHERE s.name = 'Pokemon';
+
+SELECT o.full_name, a.name AS animal_name
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id;
