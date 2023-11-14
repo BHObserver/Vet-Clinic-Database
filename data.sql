ab1def8aff4f756a5c327ce73b7972c0dda482e2
Kenneth Adutwum <kwamayadu@gmail.com>
2023-11-14 23:28:58 +0000

    Update made in data.sql

--- data.sql
+++ data.sql
@@ -1,8 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
+INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom'), ('Ditto');
 
 INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+
+INSERT INTO owners (name) VALUES ('Sam Smith'), ('Jennifer Orwell'), ('Bob'), ('Melody Pond'), ('Dean Winchester'), ('Jodie Whittaker');
 
 INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
